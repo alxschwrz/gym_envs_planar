4c32b7c27998b3d399ad741e39a184ee5d22e3f6
Alexander <alexanderschwarz.ulm@gmail.com>
2022-03-25 17:03:35 +0100

    updated rl examples script

--- examples/rl_point_robot.py
+++ examples/rl_point_robot.py
@@ -1,46 +1,66 @@
 import gym
 import planarenvs.pointRobot
 import numpy as np
-
-
-obstacles = False
-goal = True
-sensors = True
+from stable_baselines3 import PPO, SAC, DDPG
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.utils import check_for_correct_spaces
 
 
 def main():
-    env = gym.make("point-robot-vel-v0", render=True, dt=0.01)
-    defaultAction = np.array([-0.8, 0.10])
-    #defaultAction = lambda t: np.array([np.cos(1.0 * t), np.sin(1.0 * t)])
-    initPos = np.array([0.0, -1.0])
-    initVel = np.array([-1.0, 0.0])
-    n_episodes = 5
+    FLAG_VEC_NORM = False
+
+    env = gym.make("point-robot-vel-v0", render=False, dt=0.01)
+    check_env(env)
+    check_for_correct_spaces(env, env.observation_space, env.action_space)
+    env = Monitor(env)
+    env = DummyVecEnv([lambda: env])
+
+
+    eval_env = gym.make("point-robot-vel-v0", render=True, dt=0.01)
+    eval_env = Monitor(eval_env)
+    eval_env = DummyVecEnv([lambda: eval_env])
+
+    if FLAG_VEC_NORM:
+        env = VecNormalize(env, norm_obs=True, norm_reward=False,
+                           clip_obs=10.)
+        eval_env = VecNormalize(eval_env, norm_obs=True, norm_reward=False,
+                                clip_obs=10.)
+
+    #model = PPO("MultiInputPolicy", env, verbose=1, tensorboard_log="tensorboard_test/")
+    #model = PPO("MultiInputPolicy", env, verbose=1)
+
+    model = SAC('MultiInputPolicy', env, verbose=1)
+    #model = SAC('MultiInputPolicy', env, verbose=2, tensorboard_log="tensorboard_test/")
+
+    print('starting training...')
+    model.learn(total_timesteps=1_000_000, log_interval=4, tb_log_name="PPO_Rand")
+
+    #model.save("models/PPO_TESTING1")
+    #del model
+    #model = PPO.load("models/PPO_TESTING1")
+    model = SAC.load("models/SAC_TESTING")
+
+    n_episodes = 20
     n_steps = 1000
     cumReward = 0.0
+
     for e in range(n_episodes):
-        ob = env.reset(pos=initPos, vel=initVel)
+        # ob = env.reset(pos=initPos, vel=initVel)
+        ob = eval_env.reset()
 
-        if sensors:
-            from sensors.GoalSensor import GoalSensor
-            goalDistObserver = GoalSensor(nbGoals=1, mode='distance')
-            env.addSensor(goalDistObserver)
-            goalPosObserver = GoalSensor(nbGoals=1, mode='position')
-            env.addSensor(goalPosObserver)
-
-        if goal:
-            from examples.goal import splineGoal
-
-            env.addGoal(splineGoal)
         print("Starting episode")
-        t = 0
+        # t = 0
         for i in range(n_steps):
-            t = t + env.dt()
-            action = env.action_space.sample()
-            action = defaultAction#(t)
-            ob, reward, done, info = env.step(action)
+            # t = t + eval_env.dt()
+            defaultAction = np.array([0.19, 0.50])
+            # action = defaultAction  # (t)
+            action, _states = model.predict(ob, deterministic=True)
+            ob, reward, done, info = eval_env.step(action)
             if (i % 100) == 1:
-                print("it: {}, obs: {}, reward: {}".format(i, ob, reward))
-                #print(ob, reward, done, info)
+                print("it: {}, act: {}, obs: {}, reward: {}".format(i, action, ob, reward))
+                # print(ob, reward, done, info)
             cumReward += reward
             if done:
                 break
